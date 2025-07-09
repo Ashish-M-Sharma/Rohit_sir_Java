d8b68ec3ff86370c7744262b90021c2d72d51292
Ashish-M-Sharma <your-email@example.com>
2025-07-09 15:42:24 +0530

    "first commit"

--- UserInput.java
+++ UserInput.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 class UserInput {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
         Scanner sc = new Scanner(System.in);
         int a, b, c;
         System.out.println("Enter A:");
